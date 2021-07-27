cd9c1799cb6a37e8d731ca975cf4da1ab7ec379b
KyalSinLinLett <alexsin0012@gmail.com>
2021-07-27 11:55:21 +0630

    timestamp endpoints added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +21,31 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+// request to /api/:date? with a valid date 
+// -> { unix: milliseconds, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+app.get('/api/:date?', (req, res) => {
+  var dateParam = req.params.date
+
+  if (!dateParam) {
+    res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() })
+  }
+
+  if (dateParam.length >= 13) 
+    dateParam = parseInt(dateParam)
+
+  var date = new Date(dateParam)
+
+  if (date.toUTCString() != "Invalid Date") {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() })
+  }
+  res.json({ error : "Invalid Date" })
+})
+
+
+
 
 
 
